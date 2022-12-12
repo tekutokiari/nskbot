753ae07b1c56b30141d6ebb02e3d9cad323e5fef
tekutokiari <67433068+tekutokiari@users.noreply.github.com>
2022-12-12 02:47:07 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,1 @@
-invite_url = r"https://discord.com/api/oauth2/authorize?client_id=831632138465837086&permissions=8&scope=bot%20applications.commands"
-token = "ODMxNjMyMTM4NDY1ODM3MDg2.YHYDyA.QbWH3khaDgNsaWD-AIWqMGosJ3Q"
-token2 = "ODE5MjA2OTc1MTQwMTM0OTYy.YEjP8w.4i9H_fPhjb3irmXAow1ERxlkYNM"
-token3= "Nzc4MDMzNDI4MTMzOTcwMDIy.X7MGGA.OQRXH9et65vAmvsk32x7TER3YX8" #friendlybot
+#deleted the tokens
